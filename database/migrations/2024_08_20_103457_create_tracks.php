881dd1a9aa64daf727c97ebd49fb99d13db526e1
Arif Billah Shobuz <98076974+arifbillahshobuz507@users.noreply.github.com>
2024-08-20 17:56:20 +0600

    music database

---
+++ database/migrations/2024_08_20_103457_create_tracks.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tracks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->double('duration', 4, 2);
+            // artist relation
+            $table->foreignId('album_id')->constrained()->restrictOnDelete()->cascadeOnUpdate();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tracks');
+    }
+};
